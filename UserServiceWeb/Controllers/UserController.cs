b6fa2d4f2003582a3a1d352a69d1c2243ba299d2
Maxim Beletskiy <bm_2002@bk.ru>
2026-06-04 16:31:58 +0300

    init

---
+++ UserServiceWeb/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserServiceWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "Hello from UserController!" });
+        }
+    }
+}
---
+++ UserServiceWeb/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.OpenApi.Models;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "UserService API",
+        Version = "v1"
+    });
+});
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "UserService API v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.MapControllers();
+
+app.Run();
