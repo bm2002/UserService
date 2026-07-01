3cc18941e1f8745059ff81805d49b6275cf0ab24
Maxim Beletskiy <bm_2002@bk.ru>
2026-07-01 12:39:58 +0300

    some changes

---
+++ UserService.Domain/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; private set; }
+        public List<OrderItem> Items { get; } = new();
+        public decimal TotalAmount { get; private set; }
+
+        public void AddItem(OrderItem item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            Items.Add(item);
+            TotalAmount += item.Price * item.Quantity;
+        }
+    }
+}
---
+++ UserService.Domain/Entities/OrderItem.cs
@@ -0,0 +1,8 @@
+﻿namespace UserService.Domain.Entities
+{
+    public class OrderItem
+    {
+        public int Price { get; private set; }
+        public int Quantity { get; private set; }
+    }
+}
